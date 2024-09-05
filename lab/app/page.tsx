dd257ba60fffd8ba4e3e3e2408c5402de4f60f6f
Bridger Tower <bridgertower@gmail.com>
2024-09-05 09:29:55 -0600

    update

--- lab/app/page.tsx
+++ lab/app/page.tsx
@@ -1,32 +1,36 @@
 export default function Home() {
   return (
-    <div className="p-12 craft">
-      <h1>Heading 1</h1>
-      <h2>Heading 2</h2>
-      <h3>Heading 3</h3>
-      <h4>Heading 4</h4>
-      <h5>Heading 5</h5>
-      <h6>Heading 6</h6>
+    <div className="p-12">
+      <h1 className="ds">Heading 1</h1>
+      <h2 className="ds">Heading 2</h2>
+      <h3 className="ds">Heading 3</h3>
+      <h4 className="ds">Heading 4</h4>
+      <h5 className="ds">Heading 5</h5>
+      <h6 className="ds">Heading 6</h6>
 
-      <p>Paragraph</p>
+      <p className="ds">Paragraph</p>
 
-      <a href="#">Link</a>
+      <a href="#" className="ds">
+        Link
+      </a>
 
-      <button>Button</button>
+      <img
+        src="https://images.unsplash.com/photo-1724075682209-449fa519694b?q=80&w=3086&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="Image"
+        className="ds"
+      />
 
-      <img src="image.jpg" alt="Image" />
+      <input type="text" className="ds" />
 
-      <input type="text" />
-
-      <ul>
+      <ul className="ds">
         <li>Unordered List Item</li>
       </ul>
 
-      <ol>
+      <ol className="ds">
         <li>Ordered List Item</li>
       </ol>
 
-      <table>
+      <table className="ds">
         <thead>
           <tr>
             <th>Table Header</th>
@@ -41,20 +45,20 @@ export default function Home() {
 
       <form>
         <label>Label</label>
-        <input type="text" />
-        <textarea rows={4} cols={50}>
+        <input type="text" className="ds" />
+        <textarea rows={4} cols={50} className="ds">
           Textarea
         </textarea>
-        <select>
+        <select className="ds">
           <option>Select Option</option>
         </select>
       </form>
 
-      <div>Division</div>
-      <span>Span</span>
+      <div className="ds">Division</div>
+      <span className="ds">Span</span>
 
-      <strong>Strong</strong>
-      <em>Emphasis</em>
+      <strong className="ds">Strong</strong>
+      <em className="ds">Emphasis</em>
 
       <br />
       <hr />
