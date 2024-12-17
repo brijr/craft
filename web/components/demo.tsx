7332ef95ea143639de2ee7c2bbddf805c6f87a77
Bridger Tower <bridgertower@gmail.com>
2024-12-17 13:22:48 -0700

    wip

--- web/components/demo.tsx
+++ web/components/demo.tsx
@@ -5,18 +5,15 @@ interface Props {
 
 export const Demo = ({ children, attribute }: Props) => {
   return (
-    <section className="grid divide-x grid-cols-[8rem_3fr]">
-      <div className="p-4 bg-accent">
+    <section className="grid divide-x grid-cols-[7rem_3fr]">
+      <div className="p-3 bg-accent">
         <p className="sr-only">HTML Attribute</p>
-        <p className="!text-sm">
-          <span className="text-muted-foreground text-xs"># </span>
-          {attribute}
-        </p>
+        <p className="!text-sm">{attribute}</p>
       </div>
 
-      <div className="p-4 relative bg-background">
+      <div className="p-9 relative bg-background">
         <p className="text-muted-foreground !text-xs absolute top-2 right-2">
-          Examle {attribute}
+          Preview
         </p>
         {children}
       </div>
